17eb17df118c9b5d398241da3fccb5ff64982763
Rocky <Rocky@mogul-tech.com>
2024-03-28 10:24:57 +0800

    refactor: footer v1

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,13 @@
-import FooterText from "./footer.mdx";
+import Link from "next/link";
+import Text from "@/components/text"
+import DateText from "@/components/date-text";
 
 const Footer = () => {
   return (
     <footer className="pt-20 sm:text-sm text-xs">
-      <FooterText />
+      <Text inlineBlock>
+        &copy; 2017-<DateText /> This site is built using <Link href="https://nextjs.org/">Next.js</Link> + <Link href="https://tailwindcss.com/">Tailwind CSS</Link> and is hosted and deployed on <Link href="https://vercel.com/">Vercel</Link>.
+      </Text>
     </footer>
   );
 };
