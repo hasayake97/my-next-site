eb579781a95b1212d55cdf23f8e5608812db6083
Rocky <Rocky@mogul-tech.com>
2024-03-29 10:09:42 +0800

    style: footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import DateText from "@/components/date-text";
 
 const Footer = () => {
   return (
-    <footer className="pt-20 sm:text-sm text-xs">
+    <footer className="pt-20 pb-4 sm:text-sm text-xs">
       <Text inlineBlock>
         &copy; 2017-<DateText /> This site is built using <Link href="https://nextjs.org/">Next.js</Link> + <Link href="https://tailwindcss.com/">Tailwind CSS</Link> and is hosted and deployed on <Link href="https://vercel.com/">Vercel</Link>.
       </Text>
