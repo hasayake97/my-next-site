2b587575026529e395411bccfe3eed758cdbc4b1
Rocky <Rocky@mogul-tech.com>
2024-04-02 15:30:39 +0800

    style: set h-5 style

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -7,31 +7,31 @@ import LinkExt, { LinkExtProps } from "@/components/link-ext";
 export const useMDXComponents = (components: MDXComponents): MDXComponents => {
   return {
     h1: ({ children }) => (
-      <h1 className="sm:text-5xl lg:text-6xl font-bold md:tracking-wider text-3xl lg:mb-8 sm:mb-7 mb-5">
+      <h1 className="sm:text-5xl lg:text-6xl font-bold md:tracking-wider text-3xl lg:mb-8 sm:mb-7 mb-6">
         <Text inlineBlock>{children}</Text>
       </h1>
     ),
 
     h2: ({ children }) => (
-      <h2 className="sm:text-4xl lg:text-5xl font-bold md:tracking-wider text-2xl lg:mb-7 sm:mb-6 mb-4">
+      <h2 className="sm:text-4xl lg:text-5xl font-bold md:tracking-wider text-2xl lg:mb-7 sm:mb-6 mb-5">
         <Text inlineBlock>{children}</Text>
       </h2>
     ),
 
     h3: ({ children }) => (
-      <h3 className="sm:text-3xl lg:text-4xl font-bold md:tracking-wider text-xl lg:mb-6 sm:mb-5 mb-3">
+      <h3 className="sm:text-3xl lg:text-4xl font-bold md:tracking-wider text-xl lg:mb-6 sm:mb-5 mb-4">
         <Text inlineBlock>{children}</Text>
       </h3>
     ),
 
     h4: ({ children }) => (
-      <h4 className="sm:text-2xl lg:text-3xl font-bold md:tracking-wider text-base lg:mb-6 sm:mb-4 mb-2">
+      <h4 className="sm:text-2xl lg:text-3xl font-bold md:tracking-wider text-base lg:mb-6 sm:mb-5 mb-4">
         <Text inlineBlock>{children}</Text>
       </h4>
     ),
 
     h5: ({ children }) => (
-      <h5 className="sm:text-1xl lg:text-2xl font-bold md:tracking-wider text-sm lg:mb-6 sm:mb-3 mb-1">
+      <h5 className="sm:text-1xl lg:text-2xl font-bold md:tracking-wider text-sm lg:mb-6 sm:mb-5 mb-4">
         <Text inlineBlock>{children}</Text>
       </h5>
     ),
