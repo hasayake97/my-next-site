b039ebb8e9299c4e62cb419dac0b8f03891502be
carol.luo <hasayake97@gmail.com>
2024-04-22 00:42:48 +0800

    feat: add local image cover.jpg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
 
   return (
     <article className="w-full h-[100vh] flex flex-col lg:flex-row">
-      <header className="lg:w-[68%] lg:h-full w-full h-[30%] bg-no-repeat bg-center bg-[url('https://fs.uniandes.edu.ec/adfs/portal/illustration/illustration.jpg?id=16F3C24314ED92B9868731AFE40F13293AD255030123B39948AEF512B34C0123')]" />
+      <header className="lg:w-[68%] lg:h-full w-full h-[30%] bg-no-repeat bg-center bg-[url('/images/cover.jpg')]" />
       <section className="overflow-hidden flex-1 px-8 pt-8 sm:pt-12 lg:pt-16 pb-12 relative">
         <Content/>
         <footer className="absolute px-8 left-0 bottom-3 w-full text-xs sm:text-sm">
