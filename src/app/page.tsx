53f211de9014302fd1890ea6dbe9e2a386109a16
Rocky <rocky@nbi.ad>
2025-06-20 13:06:30 +0800

    feat: add bing.com cover v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Page = async () => {
 
   return (
     <article className="w-full h-[100vh] flex flex-col lg:flex-row">
-      <header className="lg:w-[68%] lg:h-full w-full h-[30%] bg-no-repeat bg-center bg-cover" style={{backgroundImage: `url(${imageUrl})`}}/>
+      <header className="lg:w-[68%] lg:h-full w-full h-[30%] bg-no-repeat bg-center bg-cover grayscale-[100%]" style={{backgroundImage: `url(${imageUrl})`}}/>
       <section className="overflow-hidden flex-1 px-8 pt-8 sm:pt-12 lg:pt-16 pb-12 relative">
         <Content />
         <footer className="absolute px-8 left-0 bottom-3 w-full text-xs sm:text-sm">
