4692499150f70eeb6f95bbd64d928a33ae018a77
Rocky <rocky@nbi.ad>
2025-07-08 10:18:38 +0800

    refactor(not-found): not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,6 +5,13 @@ export const metadata: Metadata = {
   description: '苦海无涯，回头是岸.',
 }
 
-const NotFound = () => <section className="p-4">404: Not Found.</section>
+const NotFound = () => {
+  return (
+    <section className="h-full p-4 flex items-center justify-center">
+      <span className="text-2xl py-1 pr-5 mr-5 border-r border-r-[#4A4B4D]">404</span>
+      <span>This page could not be found.</span>
+    </section>
+  )
+}
 
 export default NotFound
